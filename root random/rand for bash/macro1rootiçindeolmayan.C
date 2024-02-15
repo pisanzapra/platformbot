624bb222109e5a013bdb42ed83a9b13ad1289f5d
Ömer Keskiner <keskineromer9@gmail.com>
2024-02-15 07:42:39 +0000

    Upload New File

---
+++ root random/rand for bash/macro1rootiçindeolmayan.C
@@ -0,0 +1,31 @@
+void macro1()
+{
+
+ /*int randSayi;
+ int i;
+ int a;
+ 
+  srand (time(NULL));
+
+   
+   if(argc < 2){  
+      printf("Komut Satırına Herhangi Bir Argüman Girilmedi.\n");  
+   }  
+   else {
+    a = atoi(argv[1]);
+    for (i = 0; i < a; i++) 
+     
+     {
+     
+     randSayi = rand() % 10000 + 1;
+     
+     printf("%d \n", randSayi);
+     
+     }
+   }*/
+   
+  printf("Hello Root");
+
+   
+  return 0;
+}
