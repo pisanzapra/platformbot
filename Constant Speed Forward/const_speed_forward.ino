8820e7e1896315972ec2d2d9bd7043c4ff1323e6
Ö. Kaan Keskiner <15194797-keskineromer9@users.noreply.gitlab.com>
2024-02-18 15:19:40 +0000

    Upload New File -Ö

---
+++ Constant Speed Forward/const_speed_forward.ino
@@ -0,0 +1,55 @@
+char command;
+bool shouldExit = false;
+
+// Channel A
+int directionPinA = 12;
+int pwmPinA = 3;
+int brakePinA = 9;
+
+// Channel B
+int directionPinB = 13;
+int pwmPinB = 11;
+int brakePinB = 8;
+int X;
+
+/*int getRandomPWMValue() {
+    return random(100, 256); // 100 ile 255 arasında rastgele bir değer döndürür
+}*/
+
+void setup() {
+    pinMode(directionPinA, OUTPUT);
+    pinMode(pwmPinA, OUTPUT);
+    pinMode(brakePinA, OUTPUT);
+
+    pinMode(directionPinB, OUTPUT);
+    pinMode(pwmPinB, OUTPUT);
+    pinMode(brakePinB, OUTPUT);
+
+    Serial.begin(9600);
+    randomSeed(analogRead(0)); // Rastgele sayı üretimi için seed değerini ayarla
+}
+
+void loop() {
+    if (Serial.available() > 0 && !shouldExit) {
+        command = Serial.read();
+        Serial.print("Received command: ");
+        Serial.println(command);
+
+        // Motor control operations
+        switch (command) {
+            case 'F':
+                Serial.println("Forward command received");
+                digitalWrite(brakePinA, LOW);
+                digitalWrite(brakePinB, LOW);
+                digitalWrite(directionPinA, HIGH);
+                digitalWrite(directionPinB, HIGH);
+                analogWrite(pwmPinA, 250);
+                analogWrite(pwmPinB, 250);
+                break;
+
+            default:
+                // Invalid command state
+                break;
+        }
+    }
+}
